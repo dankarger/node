fe9c84a12e9b6d4f5d0b49e6b7480a65eaf7c406
dankarger <karger.dan@gmail.com>
2022-01-09 14:51:13 +0200

    3-1

---
+++ 3/3-1.mjs
@@ -0,0 +1,9 @@
+import fs from 'fs'
+import {add}  from './test.mjs'
+import {minus} from './test.mjs'
+// const add = require('./test.mjs')
+
+console.log(add(4,2));
+console.log(minus(4,2))
+
+
---
+++ 3/test.mjs
@@ -0,0 +1,15 @@
+
+
+const name = 'mike'
+// console.log('test2123123')
+
+export const add= function(a,b){
+    return a+b
+}
+
+export const minus=function(a, b){
+    return a-b
+}
+
+
+// module.exports =  calc
