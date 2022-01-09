a2ff32da27cc371ad38ad9f703a0f0db627cde96
dankarger <karger.dan@gmail.com>
2022-01-09 13:49:18 +0200

    2-1

--- 2/2-1-app.js
+++ 2/2-1-app.js
@@ -2,4 +2,6 @@ const fs = require('fs')
 
 // import * as fs from 'fs'
 // fs.appendFileSync('note.txt', ' \n this 33333')
-fs.copyFileSync('note.txt','noteCopy.txt')
+// fs.copyFileSync('note.txt','noteCopy.txt')
+// fs.renameSync('noteCopy.txt','note2Rename.txt')
+console.log(fs.readdirSync('./'))
