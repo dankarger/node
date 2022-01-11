8c4053fca06f6f1e9fd4a4429f14b3fb71cea386
dankarger <karger.dan@gmail.com>
2022-01-11 16:34:06 +0200

    7-1 api

--- 7-api/7-1-api.js
+++ 7-api/7-1-api.js
@@ -2,7 +2,7 @@
 // import request from 'request'
 const request = require('request');
 const axios = require('axios');
-const {Axios} = require("axios");
+const superagent = require('superagent')
 
 
 API_JOKE='https://geek-jokes.sameerkumar.website/api?format=json'
@@ -16,19 +16,35 @@ let joke = ''
  request(options, (err, response, body) => {
     if(!err && response.statusCode == 200)
         // console.log(body)
-     joke=JSON.parse(body)
+        console.log('request',body)
+     console.log('request',response.data)
 });
-console.log('jpk',joke)
+
 
 // axios//
 
 const getJokeAxios = async ()=> {
-    let responseAxios = await axios.get(API_JOKE, options)
-    console.log('res',responseAxios.data)
-     return responseAxios
+    try {
+        let responseAxios = await axios.get(API_JOKE, options)
+        console.log('axios',responseAxios.data)
+        return responseAxios
+    }catch (e) {
+        console.log(e)
+    }
+
 
 }
 
 const jokeAxios =  getJokeAxios()
 // console.log('r',jokeAxios)
 console.log('rrrr',jokeAxios.data)
+
+
+//super agent
+superagent.get(API_JOKE)
+    // .query({ api_key: 'DEMO_KEY', date: '2017-08-02' })
+    .end((err, res) => {
+        if (err) { return console.log(err); }
+        console.log('super',res.body);
+        // console.log(res.body);
+    });
