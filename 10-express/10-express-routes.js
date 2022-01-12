73571dc8e4b9a8b9b4d111b41bcded70f59ae6c9
dankarger <karger.dan@gmail.com>
2022-01-12 11:38:31 +0200

    10-1 express

--- 10-express/10-express-routes.js
+++ 10-express/10-express-routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const port = 3000;
-const router = express.Router()
+// const router = express.Router()
 
 
 app.get('/numbers/', function (req, res) {
